0e71117aca806174bb9f9110106108b81d529f7d
Rutkarf <bouazza.romain@mail.com>
2024-09-09 14:09:58 +0200

    Jour01:Job01

---
+++ JOUR01/Index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Javascript</title>
+
+<!-- 2solution possible -->
+    <script type="text/javascript">
+        alert("Hello Javascript!");
+    </script>
+
+<!-- 2solution possible -->
+    <script>
+        window.onload = function() {
+            alert('Hello Javascript!');
+        };
+    </script>
+
+
+</head>
+
+<body>
+
+</body>
+
+</html>
